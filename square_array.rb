748e4983f584099732386f143b2473d68ca6d56b
Henry Panora <hpanora@gmail.com>
2019-02-24 18:43:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,10 @@ def square_array(array)
   # your code here
 numbers = [1,2,3]
 number.each do |number|
-  puts math.sprt (#{numbers})
+  puts math.sqrt (#{numbers})
 end
 new_numbers = [9,10,16,25]
 new_numbers.each do |new_number|
-  puts math.sprt (#{new_numbers})
+  puts math.sqrt (#{new_numbers})
 
 end
